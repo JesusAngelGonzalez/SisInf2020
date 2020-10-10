a236c48e09caa409d7010c7e4e3d614c42fedf43
danielbenedi6 <DanielBenediGarcia@gmail.com>
2020-10-10 14:07:07 +0200

    Terminar creación de tablas

--- script_tablas.sql
+++ script_tablas.sql
@@ -11,3 +11,29 @@ CREATE TABLE web_data.usuarios (
 	nombre text NOT NULL,
 	CONSTRAINT usuarios_pk PRIMARY KEY (correo_electronico)
 );
+
+CREATE TABLE web_data.lugares (
+	nombre text NOT NULL,
+	ubicacion text NOT NULL,
+	CONSTRAINT lugares_pk PRIMARY KEY (nombre, ubicacion)
+);
+
+CREATE TABLE web_data.positivos (
+	correo_electronico	text NOT NULL,
+	fecha			timestamp NOT NULL,
+	CONSTRAINT positivos_pk PRIMARY KEY (correo_electronico, fecha),
+	CONSTRAINT correo_positivos_fk	FOREIGN KEY (correo_electronico) REFERENCES  web_data.usuarios(correo_electronico) ON DELETE CASCADE 
+);
+
+CREATE TABLE web_data.acudir (
+	id			integer NOT NULL,
+	correo_electronico	text 	NOT NULL,
+	nombre_lugar		text 	NOT NULL,
+	ubicacion		text 	NOT NULL,
+	inicio		timestamp	NOT NULL,
+	final		timestamp	NOT NULL,
+	CONSTRAINT acurdir_pk PRIMARY KEY (id),
+	CONSTRAINT correo_acudir_fk	FOREIGN KEY (correo_electronico) REFERENCES  web_data.usuarios(correo_electronico) ON DELETE CASCADE,
+	CONSTRAINT lugar_acudir_fk	FOREIGN KEY (nombre_lugar, ubicacion) REFERENCES  web_data.lugares(nombre, ubicacion) ON DELETE CASCADE,
+	CONSTRAINT fechas_ok	CHECK (inicio < final)
+);

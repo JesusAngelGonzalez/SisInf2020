4796f764658ab9f72a60df16542c2a6f7333b5cc
danielbenedi6 <DanielBenediGarcia@gmail.com>
2020-10-10 16:08:13 +0200

    PoolConnectionManager

--- fix.sql
+++ fix.sql
@@ -3,6 +3,7 @@ ALTER TABLE web_data.acudir DROP COLUMN nombre_lugar;
 ALTER TABLE web_data.acudir DROP COLUMN ubicacion;
 ALTER TABLE web_data.acudir ADD id_ubicacion numeric NOT NULL;
 ALTER TABLE web_data.lugares DROP CONSTRAINT lugares_pk;
+ALTER TABLE web_data.lugares ADD id numeric NOT NULL;
 ALTER TABLE web_data.lugares ADD CONSTRAINT lugares_pk PRIMARY KEY (id);
 ALTER TABLE web_data.acudir ADD CONSTRAINT acudir_fk FOREIGN KEY (id_ubicacion) REFERENCES web_data.lugares(id) ON DELETE CASCADE ON UPDATE CASCADE;
 

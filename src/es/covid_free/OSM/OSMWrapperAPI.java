6141ede4f5be62bb2a1822ec9f7ab1c5b980d507
alej008 <775929@unizar.es>
2020-11-18 15:15:03 +0100

    OSM

--- src/es/covid_free/OSM/OSMWrapperAPI.java
+++ src/es/covid_free/OSM/OSMWrapperAPI.java
@@ -1,36 +1,24 @@
 package es.covid_free.OSM;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
-import java.net.Authenticator;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLEncoder;
-import java.text.DecimalFormat;
-import java.text.DecimalFormatSymbols;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
-import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
+
 public class OSMWrapperAPI {
 	private static final String NOMINATIM_API = "https://nominatim.openstreetmap.org/search?q=";
 	
-	public static String getCorrectUbicacion(String name) throws IOException, ParserConfigurationException, SAXException {
-		String query = NOMINATIM_API + name + "&format=xml&addressdetails=1&namedetails=1";
+	public static String getCorrectaDireccion(String ubicacion, String lugar) throws IOException, ParserConfigurationException, SAXException {
+		String query = NOMINATIM_API + ubicacion + ", " + lugar + "&format=xml&addressdetails=1&namedetails=1";
 		
 		URL osm = new URL(query);
 		HttpURLConnection conn = (HttpURLConnection) osm.openConnection();
@@ -39,6 +27,36 @@ public class OSMWrapperAPI {
 		DocumentBuilder docBuilder = dbfac.newDocumentBuilder();
 		Document document = docBuilder.parse(conn.getInputStream());
 		
-		return document.
+		NodeList nodos = document.getElementsByTagName("place");
+		
+		if( nodos.getLength() == 0 ) {
+			System.out.println(nodos);
+			return "";
+		}
+		
+		Node info = nodos.item(0);
+		String dir = "";
+		
+		for(  int i = 0; i < info.getChildNodes().getLength(); i++) {
+			Node child = info.getChildNodes().item(i);
+			if(child.getNodeName().equals("namedetails")) {
+				//System.out.println("Name: " + child.getFirstChild().getNodeValue().getClass());
+				dir += child.getFirstChild().getNodeValue();
+			}else if(child.getNodeName().equals("road")) {
+				dir += child.getNodeValue();
+			}else  if(child.getNodeName().equals("city")) {
+				dir += child.getNodeValue();
+			}
+		}
+				
+		return dir;
+	}
+	
+	public static void main(String[] args) {
+		try {
+			System.out.println(getCorrectaDireccion("Juncos", "Zaragoza"));	
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 }

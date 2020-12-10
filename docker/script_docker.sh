b9b067def231c8d428c7d9f9490f0308b5c75336
danielbenedi6 <DanielBenediGarcia@gmail.com>
2020-12-10 18:44:36 +0100

    Cambiado el script, eliminado carpeta main y algunas carpetas

--- docker/script_docker.sh
+++ docker/script_docker.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-sudo docker build -t sisinf/tomcat:latest ./tomcat
+cd postgres && unzip main.zip && cd ..
+sudo docker stop sisinf-database
+sudo docker rm sisinf-database
 sudo docker build -t sisinf/postgres:latest ./postgres
+
+sudo docker stop sisinf-tomcat
+sudo docker rm sisinf-tomcat
+sudo docker build -t sisinf/tomcat:latest ./tomcat
 sudo docker run --name "sisinf-database" -e ALLOW_EMPTY_PASSWORD=yes -p 5432:5432 -d sisinf/postgres:latest
 sudo docker run -d --name "sisinf-tomcat" --link "sisinf-database" -p 8080:8080 sisinf/tomcat:latest
+
+echo
+echo
+
+sudo docker ps
